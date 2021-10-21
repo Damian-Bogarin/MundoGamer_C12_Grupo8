45b4ca04312bd5e3ad943d0d869857ef99c29127
DanielaCalisaya <hdaniela127c@gmail.com>
2021-10-20 21:52:48 -0500

    archivo gitignore-ruta del index.html-puerto3008

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = 3008; /* en el puerto 3008 porque somos grupo 8 :) */
+app.use(express.static('public'));
 
-console.log('hola')
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html")) /*es la pagina de inicio: home*/
+})
+
+/*
+productDetail.html
+productCart.html
+register.html
+login.html
+*/
+
+app.listen(PORT, () => 
+console.log(`Servidor levantado en el puerto ${PORT} http://localhost:${PORT}`))
