d8f1d6a4ceb13ec2cd2ae9a62b2333dc124e7231
DanielaCalisaya <hdaniela127c@gmail.com>
2021-10-31 21:29:44 -0500

    arreglo app.js y borro login.css

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
 const path = require('path');
 const PORT = 3008; /* en el puerto 3008 porque somos grupo 8 :) */
 app.use(express.static('public'));
 
+
+
 app.get("/home",(req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
-    })
+})
+
 app.get("/categoria",(req,res) => {
     res.sendFile(path.join(__dirname, '/views/categorias.html'))
-     })
+})
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html")) /*página log-in*/
+    res.sendFile(path.join(__dirname, "/views/login.html")) 
 })
 
 app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/productDetail.html")) /*página detalle de producto*/
+    res.sendFile(path.join(__dirname, "/views/productDetail.html")) 
 })
 
 app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/productCart.html")) /*página: carrito de compras*/
+    res.sendFile(path.join(__dirname, "/views/productCart.html")) 
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html")) /*página de registro*/
+    res.sendFile(path.join(__dirname, "/views/register.html")) 
 })
 
 
