cb433a272e83614853fd9c4722e75714e921429a
DanielaCalisaya <hdaniela127c@gmail.com>
2021-11-05 12:37:14 -0500

    added productCart- html and css

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
     })
+    
 app.get("/categorias",(req,res) => {
     res.sendFile(path.join(__dirname, '/views/categorias.html'))
 })
