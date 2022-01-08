86473bc5d1fb004d98d56ef0592cc72820f137bd
DanielaCalisaya <hdaniela127c@gmail.com>
2022-01-07 19:11:24 -0600

    ruta de admin

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -5,13 +5,13 @@ let router = express.Router();
 let controller = require('../controllers/adminController'); 
 let adminLogMiddlewares = require('../middlewares/adminLogMiddlewares')
 
-router.get('/', adminLogMiddlewares, controller.list); /* devuelve la vista listProduct */
+router.get('/', /* adminLogMiddlewares, */ controller.list); /* devuelve la vista listProduct */
 
-router.get('/create',adminLogMiddlewares, controller.create); /*devuelve la vista para crear un producto */
-/* router.post('/productCreate', controller.newProduct); */ /* <- todavia no esta echa...guardaria el producto en la base de datos */
+router.get('/create',/* adminLogMiddlewares, */ controller.create); /*devuelve la vista para crear un producto */
+/* router.post('/productCreate', controller.newProduct); */ 
 
-router.get('/update/:id',adminLogMiddlewares, controller.update); /* devuelve la vista en donde actualizo el producto (edito) */
-/* router.post('/updateProduct', controller.editProduct); */ /* <- todavia no esta echa... guardaria el producto actualizado, editado en la base de datos */
+router.get('/update/:id',/* adminLogMiddlewares, */ controller.update); /* devuelve la vista en donde actualizo el producto (edito) */
+/* router.post('/updateProduct', controller.editProduct); */ 
 
 
 
