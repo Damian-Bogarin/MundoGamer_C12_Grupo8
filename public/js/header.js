809a6a08888c828c3423a0436479d04b51c7eb6b
Damian-Bogarin <boga_nicdam@outlook.com>
2022-03-28 19:04:21 -0300

    Final

--- public/js/header.js
+++ public/js/header.js
@@ -16,24 +16,17 @@ function dropMenu() {
 }
 
 
+let $boton = document.querySelector(".dropbtn");
+$boton.addEventListener('click', function(){
+  NotificationFuntion()
+} )
 /* icon message */
 function NotificationFuntion() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+  
 
   window.addEventListener('load', function(){
 
